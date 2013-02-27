d3f608a993d1288a3bcd53a2834a2b3b7a0e0fc2
doctorjnupe <jerry@hybridgroup.com>
2013-02-27 13:53:31 -0600

    Update attributes.

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,8 +3,31 @@
 #
 
 default[:postgresql][:version] = "9.1"
+set[:postgresql][:dir]         = "/etc/postgresql/#{node[:postgresql][:version]}/main"
+
+default['postgresql']['config']['data_directory'] = "/var/lib/postgresql/#{node['postgresql']['version']}/main"
+default['postgresql']['config']['hba_file'] = "/etc/postgresql/#{node['postgresql']['version']}/main/pg_hba.conf"
+default['postgresql']['config']['ident_file'] = "/etc/postgresql/#{node['postgresql']['version']}/main/pg_ident.conf"
+default['postgresql']['config']['external_pid_file'] = "/var/run/postgresql/#{node['postgresql']['version']}-main.pid"
+default['postgresql']['config']['listen_addresses'] = 'localhost'
+default['postgresql']['config']['port'] = 5432
+default['postgresql']['config']['max_connections'] = 100
+default['postgresql']['config']['unix_socket_directory'] = '/var/run/postgresql'
+default['postgresql']['config']['shared_buffers'] = '24MB'
+default['postgresql']['config']['max_fsm_pages'] = 153600 if node['postgresql']['version'].to_f < 8.4
+default['postgresql']['config']['log_line_prefix'] = '%t '
+default['postgresql']['config']['datestyle'] = 'iso, mdy'
+default['postgresql']['config']['default_text_search_config'] = 'pg_catalog.english
 default[:postgresql][:ssl]     = "true"
 set[:postgresql][:dir]         = "/etc/postgresql/#{node[:postgresql][:version]}/main"
+
+default['postgresql']['client']['packages'] = %w{postgresql-client libpq-dev}
+default['postgresql']['server']['packages'] = %w{postgresql}
+default['postgresql']['contrib']['packages'] = %w{postgresql-contrib}
+
+default['postgresql']['enable_pitti_ppa'] = false
+default['postgresql']['enable_pgdg_yum'] = false
+
 
 default[:postgresql][:hba] = [
   { :method => 'md5', :address => '127.0.0.1/32' },
